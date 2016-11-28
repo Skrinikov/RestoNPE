62d39373cd847d735b1f7bd7b0850914af6e4484
jeegnathebug <jeegnathebug@gmail.com>
2016-11-28 11:07:04 -0500

    Minor fixes

--- app/src/main/java/npe/com/restonpe/MainActivity.java
+++ app/src/main/java/npe/com/restonpe/MainActivity.java
@@ -1,7 +1,13 @@
 package npe.com.restonpe;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import npe.com.restonpe.services.RestoLocationManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Request permission
+        ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, RestoLocationManager.LOCATION_MANAGER_REQUEST_CODE );
+    }
+
+    public void buttonClick(View v) {
+        Intent intent = new Intent(this, NearRestosActivity.class);
+        startActivity(intent);
     }
 }
