1141c8ea61b24cdb2323715bc847d4f562520d2c
cindy <uenyihung@gmail.com>
2016-11-30 13:24:44 -0500

    added the pictures for the about activity fragment

--- app/src/main/java/npe/com/restonpe/Fragments/AboutFragment.java
+++ app/src/main/java/npe/com/restonpe/Fragments/AboutFragment.java
@@ -1,10 +1,15 @@
 package npe.com.restonpe.Fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import npe.com.restonpe.R;
 
@@ -12,9 +17,30 @@ import npe.com.restonpe.R;
  * Created by Hung on 11/23/2016.
  */
 public class AboutFragment extends Fragment {
+    private LinearLayout dawson_ll;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_about, container, false);
+    }
+
+    /**
+     * Sets up the onclick events of the different elements.
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        dawson_ll = (LinearLayout) getActivity().findViewById(R.id.dawson_logo);
+        dawson_ll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dawsoncollege.qc.ca/"));
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/npe/com/restonpe/Fragments/SettingFragment.java
+++ app/src/main/java/npe/com/restonpe/Fragments/SettingFragment.java
@@ -1,18 +1,37 @@
 package npe.com.restonpe.Fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import npe.com.restonpe.R;
 
 /**
- * Created by Hung on 11/24/2016.
+ * Fragment class that will load the content of the SettingActivity of RestoNPE.
+ *
+ * @author Uen Yi Cindy Hung
+ * @version 1.1
+ * @since 30/11/2016
  */
 
 public class SettingFragment extends Fragment {
+
+    /**
+     * Inflates a layout to be the content layout of the SettingActivity.
+     *
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
