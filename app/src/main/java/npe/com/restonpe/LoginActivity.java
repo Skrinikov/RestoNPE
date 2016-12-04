1d9a75d4e2d67651736ddc1de197655f93449383
Skrinikov <skrinikovd@gmail.com>
2016-12-04 17:44:01 -0500

    Add some logic, unfinished

--- app/src/main/java/npe/com/restonpe/LoginActivity.java
+++ app/src/main/java/npe/com/restonpe/LoginActivity.java
@@ -1,7 +1,11 @@
 package npe.com.restonpe;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 /**
  * Creates an instance of the login activity.
@@ -14,9 +18,49 @@ import android.os.Bundle;
  */
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.i(TAG,"onCreate()");
+    }
+
+    public void attemptLogin(View view) {
+        if (validateInputs()) {
+            Log.d(TAG,"input valid, attempting to login");
+
+        }
+    }
+
+    private boolean validateInputs() {
+        boolean isValid = true;
+
+        EditText email = (EditText) findViewById(R.id.email);
+        if (email.getText().toString().trim().isEmpty()) {
+            isValid = false;
+            TextInputLayout temp = (TextInputLayout)findViewById(R.id.emailLbl);
+            temp.setErrorEnabled(true);
+            temp.setError(getString(R.string.login_email_error));
+        }
+
+        EditText name = (EditText) findViewById(R.id.email);
+        if (name.getText().toString().trim().isEmpty()) {
+            isValid = false;
+            TextInputLayout temp = (TextInputLayout)findViewById(R.id.nameLbl);
+            temp.setError(getString(R.string.login_name_error));
+            temp.setErrorEnabled(true);
+        }
+
+        EditText pass = (EditText) findViewById(R.id.email);
+        if (pass.getText().toString().trim().length() < 6) {
+            isValid = false;
+            TextInputLayout temp = (TextInputLayout)findViewById(R.id.passwordLbl);
+            temp.setError(getString(R.string.login_password_error));
+            temp.setErrorEnabled(true);
+        }
+
+        return isValid;
     }
 }
