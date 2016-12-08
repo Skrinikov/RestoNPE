9582661281885fab92817cf55b2ef9a3877a8b42
cindy <uenyihung@gmail.com>
2016-12-08 13:12:23 -0500

    I did something, got some visuals, seems like I did the good thing, however I am not sure what I just did...... Q_Q

--- app/src/main/java/npe/com/restonpe/Fragments/FindSomewhereRestoFragment.java
+++ app/src/main/java/npe/com/restonpe/Fragments/FindSomewhereRestoFragment.java
@@ -2,13 +2,22 @@ package npe.com.restonpe.Fragments;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 import npe.com.restonpe.R;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by Hung on 12/8/2016.
@@ -19,6 +28,7 @@ public class FindSomewhereRestoFragment extends Fragment {
     private static final String TAG = NearRestoFragment.class.getSimpleName();
 
     private Activity activity;
+    private int PLACE_PICKER_REQUEST = 1;
 
     /**
      * Inflates a layout to be the content layout of the FindSomewhereRestoActivity.
@@ -36,5 +46,39 @@ public class FindSomewhereRestoFragment extends Fragment {
                              Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView called");
         return inflater.inflate(R.layout.activity_find_somewhere_resto, container, false);
+    }
+
+    /**
+     * Used as reference:
+     * source: https://developers.google.com/places/android-api/placepicker
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "onActivityCreated called");
+        activity = getActivity();
+
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try {
+            startActivityForResult(builder.build(activity), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, activity);
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(activity, toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
