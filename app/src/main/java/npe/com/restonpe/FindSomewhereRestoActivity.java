d84d971f7efbf1a23b4eb6fa63fb95c63a521721
cindy <uenyihung@gmail.com>
2016-12-08 15:02:39 -0500

    didnt need the xml layout and fragment, made the find from somehwere activity into a fragment activity instead

--- app/src/main/java/npe/com/restonpe/FindSomewhereRestoActivity.java
+++ app/src/main/java/npe/com/restonpe/FindSomewhereRestoActivity.java
@@ -1,25 +1,70 @@
 package npe.com.restonpe;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-import npe.com.restonpe.Fragments.FavRestoFragment;
-import npe.com.restonpe.Fragments.FindSomewhereRestoFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
-public class FindSomewhereRestoActivity extends BaseActivity {
-    private FindSomewhereRestoFragment fragment;
+public class FindSomewhereRestoActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = FindSomewhereRestoActivity.class.getSimpleName();
+    private int PLACE_PICKER_REQUEST = 1;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setTitle(R.string.title_activity_find_from_somewhere);
-        }
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
 
-        fragment = new FindSomewhereRestoFragment();
-        super.createFragments(fragment);
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try {
+            Log.d(TAG,"before startActivityForResult for Google Picker");
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Code provided by Google.
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG,"onActivityResult called");
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            Log.d(TAG,"Place Picker Request Returned");
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(this,data);
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
