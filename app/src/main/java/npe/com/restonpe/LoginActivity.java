c7769e0ed2d8b7f715289bc4263f68900be72640
Skrinikov <skrinikovd@gmail.com>
2016-12-09 23:45:17 -0500

    Add login authentication, untested

--- app/src/main/java/npe/com/restonpe/LoginActivity.java
+++ app/src/main/java/npe/com/restonpe/LoginActivity.java
@@ -1,13 +1,24 @@
 package npe.com.restonpe;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Creates an instance of the login activity.
@@ -25,6 +36,14 @@ public class LoginActivity extends AppCompatActivity {
     EditText name;
     EditText pwd;
 
+    // Authentication Variables.
+    private static final String LOGIN_URL = "http://shrouded-thicket-29911.herokuapp.com/api/userlogin";
+    private String jsonData = "{\"email\":\"%1$s\",\"password\":\"%2$s\"}";
+
+    private static final String CONTENT_TYPE = "Content-Type";
+    private static final String TYPE = "application/json; charset=UTF-8;";
+    private static final String CONTENT_LENGTH = "Content-Length";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,27 +53,19 @@ public class LoginActivity extends AppCompatActivity {
         name = (EditText) findViewById(R.id.name);
         pwd = (EditText) findViewById(R.id.password);
 
+
         Log.i(TAG, "onCreate()");
     }
 
     /**
-     * Validates that all the fields are provided
+     * Validates that all the fields are provided, IF everything has a valid format checks with the
+     * the api to see if this user exists.
+     *
      * @param view The view upon which the event was called
      */
     public void attemptLogin(View view) {
         if (validateInputs()) {
-            Log.d(TAG, "input valid, attempting to login");
-            SharedPreferences prefs = getSharedPreferences(BaseActivity.SHARED_PREFS, MODE_PRIVATE);
-            prefs.edit()
-                    .putString(SettingActivity.USERNAME, name.getText().toString().trim())
-                    .putString(SettingActivity.EMAIL, email.getText().toString().trim())
-                    .putString(SettingActivity.PASSWORD,pwd.getText().toString().trim())
-                    .apply();
-
-            Intent intent = new Intent(this, MainActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
+            new AuthAsync().execute(email.getText().toString().trim(),pwd.getText().toString().trim());
         }
     }
 
@@ -89,5 +100,109 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return isValid;
+    }
+
+    public class AuthAsync extends AsyncTask<String, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            HttpURLConnection conn = null;
+            OutputStream out;
+            boolean isFound = false;
+
+            try {
+
+                Log.i(TAG, "Requesting: "+LOGIN_URL);
+                URL url = new URL(LOGIN_URL);
+
+                if(isNetworkAccessible()){
+                    conn = (HttpURLConnection) url.openConnection();
+
+                    // Format Json string
+                    jsonData = String.format(jsonData, params[0], params[1]);
+                    byte[] bytes = jsonData.getBytes("UTF-8");
+                    int bytesLeng = bytes.length;
+
+                    // Set HTTP request
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty(CONTENT_TYPE, TYPE);
+                    conn.addRequestProperty(CONTENT_LENGTH, String.valueOf(bytesLeng));
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
+
+                    out = new BufferedOutputStream(conn.getOutputStream());
+
+                    out.write(bytes);
+                    out.flush();
+                    out.close();
+
+                    // Get response
+                    int response = conn.getResponseCode();
+                    if (response != HttpURLConnection.HTTP_CREATED) {
+                        Log.e(TAG, "Something went wrong. The URL was " + url + " The HTTP response was " + response + " " + conn.getResponseMessage());
+                        isFound = false;
+                    } else {
+                        Log.i(TAG, "Success!");
+                        isFound = true;
+                    }
+
+                    conn.disconnect();
+                }
+
+            } catch (MalformedURLException mue) {
+                Log.e(TAG, "Malformed URL: " + LOGIN_URL);
+            } catch (IOException ioe) {
+                Log.e(TAG, "An IOException occurred while reading the JSON file: " + ioe.getMessage());
+
+            } finally {
+                if (conn != null)
+                    conn.disconnect();
+            }
+            return isFound;
+        }
+
+        @Override
+        public void onPostExecute(Boolean result){
+            if(result)
+                saveToPreferencesAndLaunchMain();
+            else
+                displayFailedLogin();
+        }
+    }
+
+    private void displayFailedLogin() {
+        Log.i(TAG, "displayFailedLogin");
+    }
+
+    /**
+     * Checks if the network is up and usable.
+     *
+     * @return {@code True} if the network is up and can be used, {@code False} otherwise
+     */
+    public boolean isNetworkAccessible() {
+        ConnectivityManager connMgr = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    /**
+     * Saves the current preferences into the database and then launches the main activity with a new
+     * task flags.
+     */
+    private void saveToPreferencesAndLaunchMain(){
+        Log.d(TAG, "input valid, attempting to login");
+        SharedPreferences prefs = getSharedPreferences(BaseActivity.SHARED_PREFS, MODE_PRIVATE);
+        prefs.edit()
+                .putString(SettingActivity.USERNAME, name.getText().toString().trim())
+                .putString(SettingActivity.EMAIL, email.getText().toString().trim())
+                .putString(SettingActivity.PASSWORD, pwd.getText().toString().trim())
+                .apply();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
